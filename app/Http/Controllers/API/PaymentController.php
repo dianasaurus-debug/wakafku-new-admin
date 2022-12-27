59fa80e32d08185f8c95084a3ffb12fdd14e6281
Diana Fitri <nadatalita76@gmail.com>
2022-12-27 07:36:52 +0700

    update detail payment

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -59,7 +59,6 @@ class PaymentController extends Controller
             if ($payment_method_data->kind == 'ewallet') {
                 $ref_id = 'wakafku-ewallet-'.time().$transaction_data->id;
                 $intended_link = config('__constant.FCM_URL_TEST');
-                return $intended_link . '/payment?id=' . $transaction_data->id . '&is_failed=false';
                 $phone_number = isset($request->phone_number) ? $request->phone_number : $wakif->phone;
                 $short_url = create_short_link($intended_link . '/payment?id=' . $transaction_data->id . '&is_failed=false');
                 $channel_property = $request->channel == 'ID_OVO' ? $phone_number : $short_url;
