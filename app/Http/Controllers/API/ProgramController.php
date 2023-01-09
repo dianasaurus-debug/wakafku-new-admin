3fd3b554062540e15e37135b5b28960a55ab4532
Diana Fitri <nadatalita76@gmail.com>
2023-01-09 09:26:15 +0700

    mass updates

--- app/Http/Controllers/API/ProgramController.php
+++ app/Http/Controllers/API/ProgramController.php
@@ -15,6 +15,7 @@ class ProgramController extends Controller
             if (isset($request->latawal) && isset($request->longawal)) {
                 if (request()->query('keyword')) {
                     $all_programs = Program::where('title', 'like', '%' . request()->query('keyword') . '%')
+                        ->orWhere('alamat_detail', 'like', '%' . request()->query('keyword') . '%')
                         ->where('status', 'approved')
                         ->with('category')
                         ->get();
