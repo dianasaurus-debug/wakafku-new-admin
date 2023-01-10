9a3273b61d483cc545449dc595189714143750a8
Diana Fitri <nadatalita76@gmail.com>
2023-01-10 18:46:25 +0700

    mass updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,11 @@ class Kernel extends ConsoleKernel
     {
         $payment_reminders = PaymentReminder::where('is_activated', true)->get();
         foreach($payment_reminders as $reminder) {
-            $schedule->command('users:notify')->everyFiveMinutes();
+            $schedule->command('users:notify')->daily()->at('7:00')->when(function () use ($reminder) {
+                return (
+                    Carbon::parse($reminder->scheduled_date) == Carbon::today()
+                );
+            });
         }
     }
 
